fc0a654bc51b2f91d0b13c0e1644c357f248cf3d
David Choloyan <shamsani.david@gmail.com>
2025-08-06 19:54:37 +0200

    fix: navbar dropdown z-index

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -34,36 +34,42 @@ const Navbar = () => {
               label='Blockchain'
               options={navigationOptions.blockchain}
               withBorder
+              wrapperClassname='z-[90]'
             />
             <Dropdown
               id='staking'
               label='Staking'
               options={navigationOptions.staking}
               withBorder
+              wrapperClassname='z-[90]'
             />
             <Dropdown
               id='governance'
               label='Governance'
               options={navigationOptions.governance}
               withBorder
+              wrapperClassname='z-[90]'
             />
             <Dropdown
               id='tokens'
               label='Tokens'
               options={navigationOptions.tokens}
               withBorder
+              wrapperClassname='z-[90]'
             />
             <Dropdown
               id='nfts'
               label='NFTs'
               options={navigationOptions.nfts}
               withBorder
+              wrapperClassname='z-[90]'
             />
             <Dropdown
               id='education'
               label='Education'
               options={navigationOptions.education}
               withBorder
+              wrapperClassname='z-[90]'
             />
             <ScreenDropdown
               id='analytics'
