5430d491114a97061806508b5273a5ba633f1fc6
David Choloyan <shamsani.david@gmail.com>
2025-10-06 20:09:40 +0200

    feat: theming migration to sdk

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,56 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [require("../cexplorer-sdk/tailwind.config")],
+  presets: [require("@vellumlabs/cexplorer-sdk/tailwind.config")],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
+    "./node_modules/@vellumlabs/cexplorer-sdk/**/*.{js,ts,tsx,mjs,cjs}",
   ],
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1440px",
-      },
-    },
-    extend: {
-      fontSize: {
-        xs: "12px",
-      },
-      fontFamily: {
-        inter: ["Inter", "sans-serif"],
-        poppins: ["Poppins", "sans-serif"],
-      },
-      colors: {
-        text: "var(--text)",
-        background: "var(--background)",
-        cardBg: "var(--cardBg)",
-        primary: "var(--primary)",
-        darkBlue: "var(--darkBlue)",
-        secondaryText: "var(--secondaryText)",
-        secondaryBg: "var(--secondaryBg)",
-        darker: "var(--darker)",
-        border: "var(--border)",
-        borderDarker: "var(--borderDarker)",
-        borderFaded: "var(--borderFaded)",
-        grayTextPrimary: "var(--grayTextPrimary)",
-        grayTextSecondary: "var(--grayTextSecondary)",
-        all: "var(--all)",
-        completed: "var(--completed)",
-        inProgress: "var(--inProgress)",
-        notStarted: "var(--notStarted)",
-        bannerGradient: "var(--bannerGradient)",
-        purpleText: "var(--purpleText)",
-        redText: "var(--redText)",
-        greenText: "var(--greenText)",
-        yellowText: "var(--yellowText)",
-        hoverHighlight: "var(--hoverHighlight)",
-        highlightBorder: "var(--highlightBorder)",
-        tableHover: "var(--tableHover)",
-      },
-    },
-  },
   plugins: [require("tailwindcss-animate")],
 };
