e8c1bed5dd15af3c3b87a762eaee78bf0a87a3d2
konopvik <konopatenkoviktor@gmail.com>
2025-10-08 10:54:14 +0200

    splitting skeleton and ada price table into different files

--- src/components/ada-price/AdaPriceTable.tsx
+++ src/components/ada-price/AdaPriceTable.tsx
@@ -16,65 +16,7 @@ import Bitcoin from "@/resources/images/wallet/bitcoin.svg";
 import { Tooltip } from "@/components/ui/tooltip";
 import { configJSON } from "@/constants/conf";
 import { lovelaceToAda } from "@/utils/lovelaceToAda";
-import LoadingSkeleton from "@/components/global/skeletons/LoadingSkeleton";
-
-const AdaPriceTableSkeleton: FC = () => {
-  return (
-    <div className='flex w-full flex-col rounded-lg border border-border bg-cardBg pb-4 pt-4 lg:min-w-[390px] lg:max-w-[400px] lg:pb-0'>
-      <div className='flex h-[110px] w-full flex-col gap-1 border-b border-border px-6 pb-4'>
-        <div className='flex items-center gap-2'>
-          <LoadingSkeleton width='36px' height='36px' rounded='md' />
-          <LoadingSkeleton width='80px' height='24px' />
-          <LoadingSkeleton width='70px' height='20px' />
-        </div>
-        <div className='flex items-center gap-2'>
-          <LoadingSkeleton width='120px' height='32px' />
-          <LoadingSkeleton width='60px' height='24px' rounded='full' />
-        </div>
-        <div className='flex items-center gap-1'>
-          <LoadingSkeleton width='40px' height='16px' />
-          <LoadingSkeleton width='14px' height='14px' />
-          <LoadingSkeleton width='30px' height='16px' />
-        </div>
-      </div>
-
-      <div className='flex h-[50px] flex-grow items-center border-b border-border px-6'>
-        <div className='flex min-w-[160px] items-center gap-1'>
-          <LoadingSkeleton width='80px' height='16px' />
-        </div>
-        <LoadingSkeleton width='100px' height='16px' />
-      </div>
-
-      <div className='flex h-[50px] flex-grow items-center border-b border-border bg-darker px-6'>
-        <div className='flex min-w-[160px] items-center gap-1'>
-          <LoadingSkeleton width='40px' height='16px' />
-        </div>
-        <LoadingSkeleton width='120px' height='16px' />
-      </div>
-
-      <div className='flex h-[50px] flex-grow items-center border-b border-border px-6'>
-        <div className='flex min-w-[160px] items-center gap-1'>
-          <LoadingSkeleton width='130px' height='16px' />
-        </div>
-        <LoadingSkeleton width='140px' height='16px' />
-      </div>
-
-      <div className='flex h-[50px] flex-grow items-center border-b border-border bg-darker px-6'>
-        <div className='flex min-w-[160px] items-center gap-1'>
-          <LoadingSkeleton width='90px' height='16px' />
-        </div>
-        <LoadingSkeleton width='100px' height='16px' />
-      </div>
-
-      <div className='flex h-[50px] flex-grow items-center border-b px-6 lg:border-none'>
-        <div className='flex min-w-[160px] items-center gap-1'>
-          <LoadingSkeleton width='80px' height='16px' />
-        </div>
-        <LoadingSkeleton width='140px' height='16px' />
-      </div>
-    </div>
-  );
-};
+import { AdaPriceTableSkeleton } from "./AdaPriceTableSkeleton";
 
 export const AdaPriceTable: FC = () => {
   const price = useAdaPriceWithHistory();
@@ -88,8 +30,6 @@ export const AdaPriceTable: FC = () => {
   const { genesisParams } = configJSON;
 
   const totalSupply = genesisParams[0].shelley[0].maxLovelaceSupply;
-
-  console.log("totalSupply", totalSupply);
 
   return (
     <div className='flex w-full flex-col rounded-lg border border-border bg-cardBg pb-4 pt-4 lg:min-w-[390px] lg:max-w-[400px] lg:pb-0'>
