482b0f18299cfbf944fee3480f1129cc2ce77122
konopvik <konopatenkoviktor@gmail.com>
2025-10-10 00:11:46 +0200

    first version of tx detail search

--- src/components/tx/tabs/ContentTabItem.tsx
+++ src/components/tx/tabs/ContentTabItem.tsx
@@ -3,6 +3,9 @@ import { useFetchTxDetail } from "@/services/tx";
 import { useTxSortStore } from "@/stores/tx/txSortStore";
 import { getRouteApi } from "@tanstack/react-router";
 import TxContentTable from "../TxContentTable";
+import { useState } from "react";
+import useDebounce from "@/hooks/useDebounce";
+import TableSearchInput from "@/components/global/inputs/SearchInput";
 
 const selectItems = [
   {
@@ -22,26 +25,46 @@ const ContentTabItem = () => {
   const query = useFetchTxDetail(hash);
   const data = query.data?.data;
 
+  const [searchQuery, setSearchQuery] = useState("");
+  const debouncedSearch = useDebounce(searchQuery, 300);
+
+  const inputsCount = data?.all_inputs?.length || 0;
+  const outputsCount = data?.all_outputs?.length || 0;
+  const showSearch = inputsCount > 10 || outputsCount > 10;
+
   return (
     <>
-      <SortBy
-        selectItems={selectItems}
-        setSelectedItem={setSort as any}
-        selectedItem={sort}
-        className='mb-2 ml-auto w-fit'
-      />
+      <div className='mb-2 flex w-full items-center gap-2'>
+        {showSearch && (
+          <TableSearchInput
+            placeholder='Search in inputs/outputs...'
+            value={searchQuery}
+            onchange={setSearchQuery}
+            showSearchIcon
+            wrapperClassName='flex-1'
+          />
+        )}
+        <SortBy
+          selectItems={selectItems}
+          setSelectedItem={setSort as any}
+          selectedItem={sort}
+          className='ml-auto w-fit'
+        />
+      </div>
       <div className='flex w-full flex-col gap-1 md:flex-row'>
         <TxContentTable
           title='Inputs'
           data={data}
           sort={sort as "value" | "index"}
           isOutput={false}
+          searchQuery={debouncedSearch}
         />
         <TxContentTable
           title='Outputs'
           data={data}
           sort={sort as "value" | "index"}
           isOutput={true}
+          searchQuery={debouncedSearch}
         />
       </div>
     </>
