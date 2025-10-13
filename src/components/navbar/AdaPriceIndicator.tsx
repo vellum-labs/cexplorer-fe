4c42faa8768fff04fd21f39f7eb55251d082c43e
David Choloyan <shamsani.david@gmail.com>
2025-10-13 10:16:16 +0200

    fix: ada price indicator border & bg colors

--- src/components/navbar/AdaPriceIndicator.tsx
+++ src/components/navbar/AdaPriceIndicator.tsx
@@ -17,7 +17,7 @@ export const AdaPriceIndicator = () => {
           <Cardano size={18} color={colors.text} />
           <span className='text-grayTextPrimary'>ADA:</span>
           <span
-            className={`border-red-500/40 bg-red-500/5 ml-0.5 flex items-center rounded-max border px-1/2 py-1/4 text-[11px] font-medium text-redText`}
+            className={`ml-0.5 flex items-center rounded-max border border-red-500/40 bg-red-500/5 px-1/2 py-1/4 text-[11px] font-medium text-redText`}
           >
             N/A
           </span>
@@ -38,7 +38,22 @@ export const AdaPriceIndicator = () => {
             })}
           </span>
           <span
-            className={`ml-0.5 flex items-center rounded-max border px-1/2 py-1/4 text-[11px] font-medium ${price.percentChange < 1 && price.percentChange > -1 ? "border-yellow-500/40 bg-yellow-500/5 text-yellowText" : price.percentChange > 0 ? "border-green-500/40 bg-green-500/5 text-greenText" : "border-red-500/40 bg-red-500/5 text-redText"}`}
+            className={`ml-0.5 flex items-center rounded-max px-1/2 py-1/4 text-[11px] font-medium ${price.percentChange < 1 && price.percentChange > -1 ? "text-yellowText" : price.percentChange > 0 ? "text-greenText" : "text-redText"}`}
+            style={{
+              border: `1px solid ${
+                price.percentChange < 1 && price.percentChange > -1
+                  ? "rgba(234, 179, 8, 0.4)"
+                  : price.percentChange > 0
+                    ? "rgba(34, 197, 94, 0.4)"
+                    : "rgba(239, 68, 68, 0.4)"
+              }`,
+              backgroundColor:
+                price.percentChange < 1 && price.percentChange > -1
+                  ? "rgba(234, 179, 8, 0.05)"
+                  : price.percentChange > 0
+                    ? "rgba(34, 197, 94, 0.05)"
+                    : "rgba(239, 68, 68, 0.05)",
+            }}
           >
             {price.percentChange > 0 && "+"}
             {price.percentChange?.toFixed(1)}%
