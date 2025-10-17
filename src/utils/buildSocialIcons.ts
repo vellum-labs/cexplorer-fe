a9f9934901d36d107f8d85c525fca011b19813d7
konopvik <konopatenkoviktor@gmail.com>
2025-10-17 17:32:34 +0200

    refactor

--- src/utils/buildSocialIcons.ts
+++ src/utils/buildSocialIcons.ts
@@ -20,55 +20,74 @@ export const buildSocialIcons = (
 
   const socialIcons: SocialIcon[] = [];
 
-  if (extended.twitter_handle) {
-    socialIcons.push({
-      icon: TwitterLogo,
-      url: `https://x.com/${extended.twitter_handle}`,
-      alt: "Twitter/X",
-    });
-  }
-  if (extended.discord_handle) {
-    socialIcons.push({
-      icon: DiscordLogo,
-      url: `https://discord.gg/${extended.discord_handle}`,
-      alt: "Discord",
-    });
-  }
-  if (extended.telegram_handle) {
-    socialIcons.push({
-      icon: TelegramLogo,
-      url: `https://t.me/${extended.telegram_handle}`,
-      alt: "Telegram",
-    });
-  }
-  if (extended.github_handle) {
-    socialIcons.push({
-      icon: GithubLogo,
-      url: `https://github.com/${extended.github_handle}`,
-      alt: "GitHub",
-    });
-  }
-  if (extended.facebook_handle) {
-    socialIcons.push({
-      icon: FacebookLogo,
-      url: `https://facebook.com/${extended.facebook_handle}`,
-      alt: "Facebook",
-    });
-  }
-  if (extended.youtube_handle) {
-    socialIcons.push({
-      icon: YoutubeLogo,
-      url: `https://youtube.com/${extended.youtube_handle}`,
-      alt: "YouTube",
-    });
-  }
-  if (extended.twitch_handle) {
-    socialIcons.push({
-      icon: TwitchLogo,
-      url: `https://twitch.tv/${extended.twitch_handle}`,
-      alt: "Twitch",
-    });
-  }
+  const socialHandleKeys: Array<keyof PoolMetaExtended> = [
+    "twitter_handle",
+    "discord_handle",
+    "telegram_handle",
+    "github_handle",
+    "facebook_handle",
+    "youtube_handle",
+    "twitch_handle",
+  ];
+
+  socialHandleKeys.forEach(key => {
+    const handle = extended[key];
+    if (!handle) return;
+
+    switch (key) {
+      case "twitter_handle":
+        socialIcons.push({
+          icon: TwitterLogo,
+          url: `https://x.com/${handle}`,
+          alt: "Twitter/X",
+        });
+        break;
+      case "discord_handle":
+        socialIcons.push({
+          icon: DiscordLogo,
+          url: `https://discord.gg/${handle}`,
+          alt: "Discord",
+        });
+        break;
+      case "telegram_handle":
+        socialIcons.push({
+          icon: TelegramLogo,
+          url: `https://t.me/${handle}`,
+          alt: "Telegram",
+        });
+        break;
+      case "github_handle":
+        socialIcons.push({
+          icon: GithubLogo,
+          url: `https://github.com/${handle}`,
+          alt: "GitHub",
+        });
+        break;
+      case "facebook_handle":
+        socialIcons.push({
+          icon: FacebookLogo,
+          url: `https://facebook.com/${handle}`,
+          alt: "Facebook",
+        });
+        break;
+      case "youtube_handle":
+        socialIcons.push({
+          icon: YoutubeLogo,
+          url: `https://youtube.com/${handle}`,
+          alt: "YouTube",
+        });
+        break;
+      case "twitch_handle":
+        socialIcons.push({
+          icon: TwitchLogo,
+          url: `https://twitch.tv/${handle}`,
+          alt: "Twitch",
+        });
+        break;
+      default:
+        break;
+    }
+  });
 
   return socialIcons;
 };
