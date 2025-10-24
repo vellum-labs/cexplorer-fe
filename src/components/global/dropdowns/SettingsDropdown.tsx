7ff49552534ddedcec551f58824a311996db75af
David Choloyan <shamsani.david@gmail.com>
2025-10-24 14:30:14 +0200

    fix: settings dropdown import

--- src/components/global/dropdowns/SettingsDropdown.tsx
+++ src/components/global/dropdowns/SettingsDropdown.tsx
@@ -4,7 +4,7 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
+} from "@vellumlabs/cexplorer-sdk";
 import { currencies } from "@/constants/currencies";
 import { locales } from "@/constants/locales";
 import { useCurrencyStore } from "@/stores/currencyStore";
