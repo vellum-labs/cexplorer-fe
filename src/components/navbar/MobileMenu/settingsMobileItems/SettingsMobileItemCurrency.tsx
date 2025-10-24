3b8087d51bd3aa2a1b3e4b6389298a73c751da3f
David Choloyan <shamsani.david@gmail.com>
2025-10-24 14:33:32 +0200

    fix: settings mobile items currency import

--- src/components/navbar/MobileMenu/settingsMobileItems/SettingsMobileItemCurrency.tsx
+++ src/components/navbar/MobileMenu/settingsMobileItems/SettingsMobileItemCurrency.tsx
@@ -7,7 +7,7 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
+} from "@vellumlabs/cexplorer-sdk";
 
 import { useCurrencyStore } from "@/stores/currencyStore";
 
