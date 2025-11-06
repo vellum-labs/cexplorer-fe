a0e93e9eff197596e1ad06e1a102a6767e1347d3
konopvik <konopatenkoviktor@gmail.com>
2025-11-06 16:52:58 +0100

    terms page

--- src/pages/article/TermsPage.tsx
+++ src/pages/article/TermsPage.tsx
@@ -1,29 +1,288 @@
-import { LoadingSkeleton } from "@vellumlabs/cexplorer-sdk";
-import { useFetchArticleDetail } from "@/services/article";
-import parse from "html-react-parser";
-import { Helmet } from "react-helmet";
+import { PageBase } from "@/components/global/pages/PageBase";
 
 export const TermsPage = () => {
-  const query = useFetchArticleDetail("en", "page", "terms");
-  const data = query.data;
-  const name = data?.name;
-
   return (
-    <>
-      <Helmet>{name && <title>{name}</title>}</Helmet>
-      <main className='flex min-h-minHeight w-full flex-col items-center p-mobile md:p-desktop'>
-        {query.isLoading ? (
-          <div className='mt-5 flex flex-col gap-5'>
-            <div className='flex flex-col items-center gap-4'>
-              <LoadingSkeleton width='150px' height='30px' />
-              <LoadingSkeleton width='160px' height='30px' />
-            </div>
-            <LoadingSkeleton width='650px' height='800px' rounded='lg' />
-          </div>
-        ) : (
-          parse(data?.data.map(item => item).join("") || "")
-        )}
-      </main>
-    </>
+    <PageBase
+      metadataOverride={{ title: "Terms & Conditions | Cexplorer.io" }}
+      title="Terms & Conditions"
+      subTitle="Updated on 18 July 2025"
+      breadcrumbItems={[{ label: "Terms & Conditions" }]}
+      adsCarousel={false}
+    >
+      <section className="flex w-full justify-center px-mobile pb-3 md:px-desktop">
+        <div className="flex max-w-[800px] flex-col">
+          <h2 className="my-2">1. Terms</h2>
+          <p className="font-light text-grayText">
+            Please read these Terms of Use ("Terms", "Terms of Use", "Terms of
+            Service") carefully before using the Cexplorer.io website (the
+            "Service") operated by Vellum Labs s.r.o. ("us", "we", or "our").
+          </p>
+          <br />
+          <p className="font-light text-grayText">
+            Your access to and use of the Service is conditioned on your
+            acceptance of and compliance with these Terms. These Terms apply to
+            all visitors, users and others who access or use the Service.
+          </p>
+          <br />
+          <p className="font-light text-grayText">
+            By accessing or using the Service you agree to be bound by these
+            Terms. If you do not agree with any of these terms, you are
+            prohibited from using or accessing this site. The materials
+            contained in this website are protected by applicable copyright and
+            trademark law.
+          </p>
+          <br />
+
+          <h2 className="my-2">2. Use License</h2>
+          <p className="font-light text-grayText">
+            Permission is granted to temporarily download one copy of the
+            materials (information or software) on our website for personal,
+            non-commercial transitory viewing only.
+          </p>
+          <br />
+          <p className="font-light text-grayText">
+            This is the grant of a license, not a transfer of title, and under
+            this license you may not:
+          </p>
+          <br />
+          <ul className="my-2 flex list-disc flex-col gap-2 pl-4">
+            <li className="font-light text-grayText">
+              Copy, modify or create derivative works of the Service or any
+              Content;
+            </li>
+            <li className="font-light text-grayText">
+              Copy, manipulate or aggregate any Content (including data) for the
+              purpose of making it available to any third party;
+            </li>
+            <li className="font-light text-grayText">
+              Trade, sell, rent, loan, lease or license any Content or access to
+              the Service, whether commercially or free of charge;
+            </li>
+            <li className="font-light text-grayText">
+              Use or introduce to the Service any data mining, crawling,
+              "scraping", robot or similar automated or data gathering or
+              extraction method, or manually access, acquire, monitor or copy
+              any portion of the Service, or download or store Content (unless
+              expressly authorized by Cexplorer.io). Certain data and other
+              information within the Service is available by subscription. Please
+              contact hello@vellumlabs.cz for more info;
+            </li>
+            <li className="font-light text-grayText">
+              Make excessive requests for information or take any action that
+              interferes with, disrupts or imposes an undue burden on the Service
+              or any server or network connected to the Service or negatively
+              affects the quality or availability of any Content, or speed or
+              functionality of the Service;
+            </li>
+            <li className="font-light text-grayText">
+              Introduce a virus, Trojan horse, worm, time bomb or other malware
+              to the Service, or use any device, software or routine to bypass
+              any software or hardware that prohibits volume requests for
+              information;
+            </li>
+            <li className="font-light text-grayText">
+              Violate, bypass or circumvent any security measure intended to
+              limit or prevent access to the Website, Content or Service; or
+              otherwise attempt to gain unauthorized access to the Service, any
+              Content or to any computer systems or networks connected to the
+              Service or any Cexplorer.io server, whether through hacking,
+              password mining, unauthorized use of another's password/credentials
+              or any other means;
+            </li>
+            <li className="font-light text-grayText">
+              Restrict, inhibit or interfere with use of the Service by any other
+              user (including by hacking or defacing the Website);
+            </li>
+            <li className="font-light text-grayText">
+              Introduce or otherwise distribute through the Website any computer
+              program that damages, interferes with, intercepts, collects,
+              releases or discloses any system, data or personal information of
+              ours or any third party;
+            </li>
+            <li className="font-light text-grayText">
+              Make use of any of our trademarks, service marks, trade names or
+              logos or those of any third party displayed on the Website; or
+              modify or remove any copyright or other proprietary notice in the
+              Content;
+            </li>
+            <li className="font-light text-grayText">
+              Use the Website, Content or Services for or in connection with any
+              activity that (i) violates any law, statute, ordinance or
+              regulation, or (ii) involves proceeds of any unlawful or illegal
+              activity.
+            </li>
+          </ul>
+          <p className="font-light text-grayText">
+            This license shall automatically terminate if you violate any of
+            these restrictions and may be terminated by us at any time. Upon
+            terminating your viewing of these materials or upon the termination
+            of this license, you must destroy any downloaded materials in your
+            possession whether in electronic or printed format.
+          </p>
+          <br />
+
+          <h2 className="my-2">3. Ownership and Intellectual Property</h2>
+          <p className="font-light text-grayText">
+            The Service contains trade marks of Cexplorer.io, including the
+            "Cexplorer.io" name and logo. All trade marks included on the Service
+            belong to Cexplorer.io have been licensed to Cexplorer.io by the
+            trade mark owner(s) for use on the Service. You are not allowed to
+            copy or otherwise use any of these trade marks in any way except as
+            set out in these terms and conditions.
+          </p>
+          <br />
+          <p className="font-light text-grayText">
+            You acknowledge and agree that the text, information, data, graphics,
+            images, audio, video, design, organization, compilation, meta data,
+            look and feel, advertising and all other protectable intellectual
+            property available on the Site and/or through the Services is the
+            Intellectual Property of Cexplorer.io, and/or its advertisers or
+            Information Providers, and is protected by intellectual property
+            laws.
+          </p>
+          <br />
+
+          <h2 className="my-2">4. Disclaimer</h2>
+          <p className="font-light text-grayText">
+            The materials on our website are provided on an 'as is' basis. We
+            make no warranties, expressed or implied, and hereby disclaim and
+            negate all other warranties including, without limitation, implied
+            warranties or conditions of merchantability, fitness for a particular
+            purpose, or non-infringement of intellectual property or other
+            violation of rights.
+          </p>
+          <br />
+          <p className="font-light text-grayText">
+            Further, we do not warrant or make any representations concerning the
+            accuracy, likely results, or reliability of the use of the materials
+            on our website or otherwise relating to such materials or on any
+            sites linked to this site.
+          </p>
+          <br />
+          <p className="font-light text-grayText">
+            By continuing to use our service, you agree to have understood the
+            additional Full Disclaimer in regard to Accuracy of Information, No
+            Investment Advice and Independence.
+          </p>
+          <br />
+          <p className="font-light text-grayText">
+            <b>
+              No Investment Advice The information provided on this website does
+              not constitute investment advice, financial advice, trading advice,
+              or any other sort of advice and you should not treat any of the
+              website's content as such. Cexplorer.io does not recommend how many
+              ADA should be bought, sold, or held by you. Do conduct your own due
+              diligence and consult your financial advisor before making any
+              investment decisions. We can help you with better orientation in
+              data and guide you during whole delegating process. After all the
+              final decision about what pool to choose is up to you.
+            </b>
+          </p>
+          <br />
+
+          <h2 className="my-2">5. Limitations of Liability</h2>
+          <p className="font-light text-grayText">
+            In no event shall we or our suppliers, subsidiaries, affiliates,
+            shareholders, directors, officers, employees, agents, partners and
+            Information Providers be liable for any damages (including, without
+            limitation, damages for loss of profit, exemplary or special damages,
+            loss of sales, loss of revenue, loss of savings, loss of goodwill,
+            loss of any software or data, loss of bargain, loss of opportunity,
+            loss of use of computer equipment, loss of or waste of management or
+            other staff time, due to business interruption and/or any indirect,
+            consequential or special loss) however arising out of the use or
+            inability to use the materials on our website, even if we or a
+            Cexplorer.io authorized representative has been notified orally or in
+            writing of the possibility of such damage.
+          </p>
+          <br />
+          <p className="font-light text-grayText">
+            If any applicable authority holds any portion of this clause to be
+            unenforceable, then the liability will be limited to the fullest
+            possible extent permitted by applicable law.
+          </p>
+          <br />
+
+          <h2 className="my-2">6. Links To Third Party Websites</h2>
+          <p className="font-light text-grayText">
+            Our Service may contain links to third-party web sites or services
+            that are not owned or controlled by Cexplorer.io.
+          </p>
+          <br />
+          <p className="font-light text-grayText">
+            Cexplorer.io has no control over, and assumes no responsibility for,
+            the content, privacy policies, or practices of any third party web
+            sites or services. You further acknowledge and agree that
+            Cexplorer.io shall not be responsible or liable, directly or
+            indirectly, for any damage or loss caused or alleged to be caused by
+            or in connection with use of or reliance on any such content, goods
+            or services available on or through any such web sites or services.
+          </p>
+          <br />
+          <p className="font-light text-grayText">
+            We strongly advise you to read the terms and conditions and privacy
+            policies of any third party web sites or services that you visit.
+          </p>
+          <br />
+
+          <h2 className="my-2">7. Termination</h2>
+          <p className="font-light text-grayText">
+            We may terminate or suspend access to our Service immediately,
+            without prior notice or liability, for any reason whatsoever,
+            including without limitation if you breach the Terms.
+          </p>
+          <br />
+          <p className="font-light text-grayText">
+            All provisions of the Terms which by their nature should survive
+            termination shall survive termination, including, without limitation,
+            ownership provisions, warranty disclaimers, indemnity and limitations
+            of liability.
+          </p>
+          <br />
+
+          <h2 className="my-2">8. Governing Law</h2>
+          <p className="font-light text-grayText">
+            These Terms shall be governed and construed in accordance with the
+            laws of Czech republic without regard to its conflict of law
+            provisions.
+          </p>
+          <br />
+          <p className="font-light text-grayText">
+            Our failure to enforce any right or provision of these Terms will not
+            be considered a waiver of those rights. If any provision of these
+            Terms is held to be invalid or unenforceable by a court, the
+            remaining provisions of these Terms will remain in effect.
+          </p>
+          <br />
+          <p className="font-light text-grayText">
+            These Terms constitute the entire agreement between us regarding our
+            Service, and supersede and replace any prior agreements we might have
+            between us regarding the Service.
+          </p>
+          <br />
+
+          <h2 className="my-2">9. Changes</h2>
+          <p className="font-light text-grayText">
+            We reserve the right, at our sole discretion, to modify or replace
+            these Terms at any time and without prior notice. What constitutes a
+            material change will be determined at our sole discretion.
+          </p>
+          <br />
+          <p className="font-light text-grayText">
+            By continuing to access or use our Service, you agree to be bound by
+            the revised terms. If you do not agree to the new terms, please stop
+            using the Service.
+          </p>
+          <br />
+
+          <h2 className="my-2">10. Contact Us</h2>
+          <p className="font-light text-grayText">
+            If you have any questions about these Terms, please contact us at
+            hello@vellumlabs.cz.
+          </p>
+          <br />
+        </div>
+      </section>
+    </PageBase>
   );
 };
