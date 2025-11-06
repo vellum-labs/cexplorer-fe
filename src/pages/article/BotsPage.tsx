accbde2aecdbdbbf574be52cca94948cd401bb1f
konopvik <konopatenkoviktor@gmail.com>
2025-11-06 18:56:27 +0100

    bounty page

--- src/pages/article/BotsPage.tsx
+++ src/pages/article/BotsPage.tsx
@@ -45,83 +45,77 @@ export const BotsPage = () => {
       adsCarousel={false}
     >
       <section className='flex w-full max-w-desktop flex-col items-center gap-4 px-mobile pb-3 md:px-desktop'>
-            {bots.map(item => (
-              <div
-                key={item.key}
-                className='flex w-full max-w-[800px] flex-col justify-between gap-1 rounded-m border border-border px-mobile py-mobile sm:flex-row sm:items-center sm:gap-0 md:px-desktop'
-              >
-                <div className='flex max-w-[410px] flex-col gap-1.5'>
-                  <div className='flex w-full flex-wrap gap-1'>
-                    {item.tags.map(tag => (
-                      <div
-                        key={tag}
-                        className='flex items-center gap-[6px] rounded-s border border-border px-[6px] py-[2px]'
-                      >
-                        {tag === "Live" && (
-                          <div className='relative'>
-                            <PulseDot />
-                          </div>
-                        )}
-                        {tag === "Coming soon" && (
-                          <div className='relative'>
-                            <PulseDot color='bg-yellowText' />
-                          </div>
-                        )}
-                        <span className='text-text-xs font-medium'>{tag}</span>
+        {bots.map(item => (
+          <div
+            key={item.key}
+            className='flex w-full max-w-[800px] flex-col justify-between gap-1 rounded-m border border-border px-mobile py-mobile sm:flex-row sm:items-center sm:gap-0 md:px-desktop'
+          >
+            <div className='flex max-w-[410px] flex-col gap-1.5'>
+              <div className='flex w-full flex-wrap gap-1'>
+                {item.tags.map(tag => (
+                  <div
+                    key={tag}
+                    className='flex items-center gap-[6px] rounded-s border border-border px-[6px] py-[2px]'
+                  >
+                    {tag === "Live" && (
+                      <div className='relative'>
+                        <PulseDot />
                       </div>
-                    ))}
-                  </div>
-                  <div className='flex flex-grow items-center justify-between'>
-                    <div className='flex flex-col justify-between gap-1/2 self-start text-wrap'>
-                      <div className='flex items-center gap-1'>
-                        <img
-                          src={item.icon}
-                          alt='BotIcon'
-                          height={24}
-                          width={24}
-                        />
-                        <h3>{item.title}</h3>
+                    )}
+                    {tag === "Coming soon" && (
+                      <div className='relative'>
+                        <PulseDot color='bg-yellowText' />
                       </div>
-                      <span className='text-grayTextPrimary'>
-                        {item.description}
-                      </span>
-                    </div>
+                    )}
+                    <span className='text-text-xs font-medium'>{tag}</span>
                   </div>
+                ))}
+              </div>
+              <div className='flex flex-grow items-center justify-between'>
+                <div className='flex flex-col justify-between gap-1/2 self-start text-wrap'>
+                  <div className='flex items-center gap-1'>
+                    <img src={item.icon} alt='BotIcon' height={24} width={24} />
+                    <h3>{item.title}</h3>
+                  </div>
+                  <span className='text-grayTextPrimary'>
+                    {item.description}
+                  </span>
                 </div>
-                {item.link ? (
-                  <Button
-                    label='Folow'
-                    rightIcon={<ArrowRight size={18} />}
-                    size='md'
-                    variant='primary'
-                  />
-                ) : (
-                  <Button
-                    label='Coming Soon'
-                    rightIcon={<BellRing size={18} />}
-                    size='md'
-                    variant='tertiary'
-                  />
-                )}
               </div>
-            ))}
-            <div className='flex max-w-[800px] flex-col gap-4 text-wrap pb-5'>
-              <div className='w-full text-center'>
-                <h1>Bots and Automation</h1>
-              </div>
-              <p className='text-start text-text-lg text-grayTextPrimary'>
-                At Cexplorer, we prioritize fair and responsible use of our
-                platform. As such, the use of automated bots or scripts to
-                crawl, scrape, or interact with our website and API is strictly
-                prohibited. Our API is intended for genuine user interaction,
-                and any automated access, including bot usage, is not allowed.
-              </p>
-              <p className='text-start text-text-lg text-grayTextPrimary'>
-                For any questions or concerns regarding our policy, feel free to
-                contact us directly.
-              </p>
             </div>
-        {/* // TODO? {parse(data?.data.map(item => item).join("") || "")} */}
+            {item.link ? (
+              <Button
+                label='Folow'
+                rightIcon={<ArrowRight size={18} />}
+                size='md'
+                variant='primary'
+              />
+            ) : (
+              <Button
+                label='Coming Soon'
+                rightIcon={<BellRing size={18} />}
+                size='md'
+                variant='tertiary'
+              />
+            )}
+          </div>
+        ))}
+        <div className='flex max-w-[800px] flex-col gap-4 text-wrap pb-5'>
+          <div className='w-full text-center'>
+            <h1>Bots and Automation</h1>
+          </div>
+          <p className='text-start text-text-lg text-grayTextPrimary'>
+            At Cexplorer, we prioritize fair and responsible use of our
+            platform. As such, the use of automated bots or scripts to crawl,
+            scrape, or interact with our website and API is strictly prohibited.
+            Our API is intended for genuine user interaction, and any automated
+            access, including bot usage, is not allowed.
+          </p>
+          <p className='text-start text-text-lg text-grayTextPrimary'>
+            For any questions or concerns regarding our policy, feel free to
+            contact us directly.
+          </p>
+        </div>
       </section>
     </PageBase>
   );
