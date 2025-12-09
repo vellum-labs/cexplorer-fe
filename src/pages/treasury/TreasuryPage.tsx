4a81ae2d20bb120fd1c8bbd307da76c0627aec15
konopvik <konopatenkoviktor@gmail.com>
2025-12-09 21:32:20 +0100

    fix: refactor treasury page

--- src/pages/treasury/TreasuryPage.tsx
+++ src/pages/treasury/TreasuryPage.tsx
@@ -1,26 +1,14 @@
 import { PageBase } from "@/components/global/pages/PageBase";
-import { useFetchAddressDetail } from "@/services/address";
+import { TREASURY_WALLETS } from "@/constants/treasury";
 import { useGetMarketCurrency } from "@/hooks/useGetMarketCurrency";
+import { useFetchAddressDetail } from "@/services/address";
 import {
-  formatString,
-  formatNumberWithSuffix,
   Copy,
+  formatNumberWithSuffix,
+  formatString,
   GlobalTable,
 } from "@vellumlabs/cexplorer-sdk";
 import { Link } from "@tanstack/react-router";
-
-const TREASURY_WALLETS = [
-  {
-    name: "Operations, hot wallet",
-    address:
-      "addr1q9sz5kw40pmnkcmmfvssm5fy2vzkk7l0wu5szv25nnffkqnkc35qyrgnqu8tl96u5eejytgvtsqatr2ms6hrxhdzq4pslvp2rm",
-  },
-  {
-    name: "Company treasury wallet",
-    address:
-      "addr1q9f9zr2ts4pzx0v3m9e2tkl6uyefrlhz39cdm3257a935efcupfwzfyl6n8szftvejs46h6697f2uvmfslq47m6vz9psek28vr",
-  },
-];
 
 interface WalletRowData {
   name: string;
