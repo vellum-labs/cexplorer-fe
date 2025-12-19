06fa728ed849e9c49a2d76f3602a683fef5657bb
konopvik <konopatenkoviktor@gmail.com>
2025-12-19 09:54:07 +0100

    fix: asap fix cheatsheet

--- src/constants/poolDebug.ts
+++ src/constants/poolDebug.ts
@@ -5,10 +5,3 @@ cabal update git fetch --tags --all
 git pull
 git checkout $(curl -s https://api.github.com/repos/input-output-hk/cardano-node/releases/latest | jq -r .tag_name)
 cabal build cardano-node cardano-cli`;
-
-export const KES_UPDATE_CODE = `cardano-cli node issue-op-cert \\
---kes-verification-key-file kes.vkey \\
---cold-signing-key-file cold.skey \\
---operational-certificate-issue-counter cold.counter \\
---kes-period 1330 \\
---out-file node.cert`;
