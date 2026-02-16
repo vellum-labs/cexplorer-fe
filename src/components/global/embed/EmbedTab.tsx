2d30af7df67e846aa426de9b576e2621013a24e3
David Choloyan <shamsani.david@gmail.com>
2026-02-16 13:16:32 +0100

    feat: detail embeds

--- src/components/global/embed/EmbedTab.tsx
+++ src/components/global/embed/EmbedTab.tsx
@@ -1,10 +1,19 @@
 import type { FC } from "react";
+
 import { Tabs } from "@vellumlabs/cexplorer-sdk";
 import { EmbedButtonsSubtab } from "./EmbedButtonsSubtab";
+import { EmbedEntity } from "./EmbedEntity";
+
 import { useAppTranslation } from "@/hooks/useAppTranslation";
 
+import {
+  ALLOWED_PARAMS,
+  EMBED_DATA_DOMAIN,
+  EMBED_TYPES,
+} from "@/constants/embed";
+
 interface EmbedTabProps {
-  entityType: "pools" | "dreps";
+  entityType: "pools" | "dreps" | "tokens";
   entityId: string;
   displayName: string;
 }
@@ -16,15 +25,67 @@ export const EmbedTab: FC<EmbedTabProps> = ({
 }) => {
   const { t } = useAppTranslation("pages");
 
+  const bannersConfig = {
+    pools:
+      ALLOWED_PARAMS["config"][EMBED_TYPES.BANNERS][EMBED_DATA_DOMAIN.POOL],
+    dreps:
+      ALLOWED_PARAMS["config"][EMBED_TYPES.BANNERS][EMBED_DATA_DOMAIN.DREP],
+    tokens:
+      ALLOWED_PARAMS["config"][EMBED_TYPES.BANNERS][EMBED_DATA_DOMAIN.TOKEN],
+  };
+
   const embedTabs = [
+    ...(entityType !== "tokens"
+      ? [
+          {
+            key: "buttons",
+            label: t(`${entityType}.detailPage.embed.buttons`),
+            content: (
+              <EmbedButtonsSubtab
+                entityType={entityType}
+                entityId={entityId}
+                displayName={displayName}
+              />
+            ),
+            visible: true,
+          },
+        ]
+      : []),
     {
-      key: "buttons",
-      label: t(`${entityType}.detailPage.embed.buttons`),
+      key: "banners",
+      label: t(`${entityType}.detailPage.embed.banners`),
       content: (
-        <EmbedButtonsSubtab
-          entityType={entityType}
+        <EmbedEntity
           entityId={entityId}
-          displayName={displayName}
+          entityType={entityType}
+          config={bannersConfig[entityType]}
+          embedType={EMBED_TYPES.BANNERS}
+        />
+      ),
+      visible: true,
+    },
+    {
+      key: "simpleLinks",
+      label: t(`${entityType}.detailPage.embed.simpleLinks`),
+      content: (
+        <EmbedEntity
+          entityId={entityId}
+          entityType={entityType}
+          config={ALLOWED_PARAMS["config"][EMBED_TYPES.SIMPLE_LINK]}
+          embedType={EMBED_TYPES.SIMPLE_LINK}
+        />
+      ),
+      visible: true,
+    },
+    {
+      key: "graphs",
+      label: t(`${entityType}.detailPage.embed.graphs`),
+      content: (
+        <EmbedEntity
+          entityId={entityId}
+          entityType={entityType}
+          config={ALLOWED_PARAMS["config"][EMBED_TYPES.GRAPH]}
+          embedType={EMBED_TYPES.GRAPH}
         />
       ),
       visible: true,
